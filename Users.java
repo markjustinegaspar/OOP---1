f865b2a3b9fc6b0e6c79b54736efa3253925ec96
markjustinegaspar <markjustine.gaspar@normi.edu.ph>
2025-10-18 06:01:54 +0800

    Update Users.java

--- Users.java
+++ Users.java
@@ -1,1 +1,45 @@
+import java.util.ArrayList;
+
+public class User extends Person {
+    private String password;
+    private String role;
+    private ArrayList<String> borrowedBooks = new ArrayList<>();
+
+    public User(String id, String name, String password, String role) {
+        super(id, name);
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public ArrayList<String> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void borrowBook(String bookId) {
+        if (!borrowedBooks.contains(bookId) && borrowedBooks.size() < 3) {
+            borrowedBooks.add(bookId);
+        }
+    }
+
+    public void returnBook(String bookId) {
+        borrowedBooks.remove(bookId);
+    }
+
+    public boolean canBorrowMore() {
+        return borrowedBooks.size() < 3;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("User ID: " + id + " | Name: " + name + " | Role: " + role);
+    }
+}
 
