a907aaf75a8b332af20c45f70a6785317fac891e
markjustinegaspar <markjustine.gaspar@normi.edu.ph>
2025-10-18 06:03:46 +0800

    Add Person class with constructor and methods

--- Person.java
+++ Person.java
@@ -1,1 +1,22 @@
 
+public class Person {
+    protected String id;
+    protected String name;
+
+    public Person(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void displayInfo() {
+        System.out.println("ID: " + id + ", Name: " + name);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
